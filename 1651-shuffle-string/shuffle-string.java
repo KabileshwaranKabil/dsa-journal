5db4c8ae580e38574a099885129a514686af6c33
M.Kabileshwaran <kabileshwaran1896@gmail.com>
2026-01-18 11:06:39 +0530

    Time: 1 ms (58.70%) | Memory: 45.7 MB (45.52%) - LeetSync

--- 1651-shuffle-string/shuffle-string.java
+++ 1651-shuffle-string/shuffle-string.java
@@ -1,18 +1,10 @@
 class Solution {
     public String restoreString(String s, int[] indices) {
         int n=s.length();
-        StringBuilder sb=new StringBuilder("");
+        char[] sh=s.toCharArray();
         for(int i=0;i<n;i++){
-            sb.append(s.charAt(search(indices,i)));
+            sh[indices[i]]=s.charAt(i);
         }
-        return sb.toString();
-    }
-    static int search(int[] indices,int target){
-        for(int i=0;i<indices.length;i++){
-            if(indices[i]==target){
-                return i;
-            }
-        }
-        return -1;
+        return new String(sh);
     }
 }
